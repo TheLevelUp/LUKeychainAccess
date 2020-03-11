b0c98b1ef909e974643da200bf977d4ab43f691a
Clay Bridges <claybridges@gmail.com>
2020-03-11 17:29:44 -0400

    Add NSKeyedUnarchiver.unarchiveObjectOfClass:withData method

--- LUKeychainAccess/NSKeyedUnarchiver+Additions.h
+++ LUKeychainAccess/NSKeyedUnarchiver+Additions.h
@@ -4,7 +4,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSKeyedUnarchiver (Additions)
 
-+ (nullable id)lu_unarchiveObjectWithData:(NSData *)data;
++ (nullable id)lu_unarchiveObjectOfClass:(nonnull Class)objectClass
+                                withData:(NSData *)data
+NS_SWIFT_NAME(unarchiveObject(class:data:)) API_AVAILABLE(ios(11), watchos(4));
+
++ (nullable id)lu_unarchiveObjectWithData:(NSData *)data
+NS_SWIFT_NAME(unarchiveObject(data:));
 
 @end
 
--- LUKeychainAccess/NSKeyedUnarchiver+Additions.m
+++ LUKeychainAccess/NSKeyedUnarchiver+Additions.m
@@ -2,16 +2,26 @@
 
 @implementation NSKeyedUnarchiver (Additions)
 
++ (nullable id)lu_unarchiveObjectOfClass:(nonnull Class)objectClass withData:(NSData *)data {
+  NSError *error;
+  id result;
+
+  if (@available(iOS 11, watchOS 4, *)) {
+    result = [self unarchivedObjectOfClass:objectClass fromData:data error:&error];
+  } else {
+    error = [NSError errorWithDomain:NSCocoaErrorDomain code:0 userInfo:nil];
+  }
+
+  if(error) {
+    [NSException raise:@"Unarchiver Error" format:@"NSKeyedUnarchiver encountered %@", error];
+  }
+
+  return result;
+}
+
 + (nullable id)lu_unarchiveObjectWithData:(NSData *)data {
-  if (@available(iOS 11, *)) {
-    NSError *error;
-    id result = [self unarchivedObjectOfClass:NSObject.class fromData:data error:&error];
-
-    if(error) {
-      [NSException raise:@"Unarchiver Error" format:@"NSKeyedUnarchiver encountered %@", error];
-    }
-
-    return result;
+  if (@available(iOS 11, watchOS 4, *)) {
+    return [self lu_unarchiveObjectOfClass:NSObject.class withData:data];
   } else {
     @try {
       return [self unarchiveObjectWithData:data];
