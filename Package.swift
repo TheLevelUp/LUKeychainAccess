a5994c512a1d608b7dbc1b74970bf240eddd2e57
Adrian Sergheev <sergheevdev@icloud.com>
2022-02-15 09:44:17 +0100

    swiftpm-support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LUKeychainAccess",
+    platforms: [
+      .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LUKeychainAccess",
+            targets: ["LUKeychainAccess"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "LUKeychainAccess",
+            dependencies: [])
+    ]
+)
